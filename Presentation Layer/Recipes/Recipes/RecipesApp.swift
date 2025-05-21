1de2ab8522e3f1209baa8ef2952830f305154764
Tak Mazarura <tm.dev12@outlook.com>
2025-05-21 12:22:44 +0100

    Created recipes client and app

---
+++ Presentation Layer/Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Tak Mazarura on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Presentation Layer/Recipes/RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Tak Mazarura on 21/05/2025.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
